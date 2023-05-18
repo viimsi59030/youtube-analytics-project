d3be61f07856cda6cd4110b667b85b15db0bbfe4
QIC Bel <tsyplakov-a@list.ru>
2023-05-18 21:18:26 +0300

    added files

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,19 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+from helper.youtube_api_manual import channel_id
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute(), indent=2, ensure_ascii=False))
